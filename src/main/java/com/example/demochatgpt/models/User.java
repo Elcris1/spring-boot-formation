6350f4a3df074bf7057e4aa95080baed376674b0
Elcris1 <eloicp10@gmail.com>
2026-04-08 12:02:00 +0200

    FIRST STEPS

---
+++ src/main/java/com/example/demochatgpt/models/User.java
@@ -0,0 +1,34 @@
+package com.example.demochatgpt.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    @Getter @Setter
+    private long id;
+
+
+    @Column(name = "email", nullable = false, unique = true, length = 100)
+    @Email(message = "Email not valid")
+    @Getter @Setter
+    private String email;
+
+    @Column(name = "password")
+    @Getter @Setter
+    private String password;
+
+    @Column(name =  "created_at")
+    @Getter @Setter
+    private Long createdAt;
+
+    @PrePersist
+    private void saveCreatedAt() {
+        createdAt = System.currentTimeMillis();
+    }
+
+}
